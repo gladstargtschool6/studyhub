084bdb1a837d1275a921b9db5e2d06e7ea32c4d6
Salma Elshaelshahawy <salma71@gmail.com>
2018-05-12 15:01:51 -0400

    add style to homepage. add responsive

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_11_160225) do
+ActiveRecord::Schema.define(version: 2018_05_12_151438) do
 
   create_table "answers", force: :cascade do |t|
     t.text "content"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 2018_05_11_160225) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
