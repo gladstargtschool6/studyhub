77082da4c8a6a752652c6dce0ad9ba804ea56cac
Salma Elshaelshahawy <salma71@gmail.com>
2018-05-13 17:38:58 -0400

    create new category form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,12 @@ class CategoriesController < ApplicationController
     def show
         @category = Category.find(params[:id])
     end
+    def new
+        
+    end
+
+    private 
+    def category_params
+       params.permit(:name)         
+    end
 end
