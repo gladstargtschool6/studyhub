6bd7b111eadad9d4fa1208c10843823873a54119
Salma Elshaelshahawy <salma71@gmail.com>
2018-05-17 14:55:43 -0400

    fix github sign in but show no name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+#   protect_from_forgery with: :exception
+  before_action :authenticate_user!
 
     def redirect_if_not_signed_in
         redirect_to root_path if !user_signed_in?
