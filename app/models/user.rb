58ab63d4cb9e82f6ee1f92c0ec95dc991b10613a
Salma Elshaelshahawy <salma71@gmail.com>
2018-05-17 21:42:20 -0400

    fix the omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
     def self.from_omniauth(auth)  
+      # binding.pry
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
+      user.name = auth.info.name
       user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
@@ -22,7 +24,7 @@ class User < ApplicationRecord
     validates :name, presence: true, uniqueness: true, length: { minimum: 4 }
     validates :email, presence: true, uniqueness: true, format: EMAIL_REGEX, length: { minimum: 4 }
     validates :password, presence: true, length: { minimum: 6 }
-    validates :password_confirmation, presence: true, length: { minimum: 6 } 
+    # validates :password_confirmation, presence: true, length: { minimum: 6 } 
 
 
 
